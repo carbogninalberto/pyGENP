a704b9ab51038e728b1d981c8cf59201d2e945a2
carbogninalberto <alberto.carbognin@studenti.unitn.it>
2022-01-14 01:49:36 +0100

    WIP compactor

---
+++ compactor.py
@@ -0,0 +1,141 @@
+import os
+import re
+
+
+REGEX_DECLARE_INT = r'(?<=int\s)(.*)(?=\s=\s.*;)'
+REGEX_DECLARE_FLOAT = r'(?<=float\s)(.*)(?=\s=\s.*;)'
+
+REGEX_USAGE_ASSIGNMENT = '.*(?<=\s=\s)({})(?=.*;)'
+REGEX_USAGE_IF_THEN_ELSE = 'if\s\(.*({})'
+
+REGEX_IS_IF = '^if\s\(.*.*\).*$'
+
+REGEX_EXPRESSION = '\((\(?\d+.?\d*\)*(\*|\+|\-|\/)?)+'
+
+lines = []
+output_lines = []
+
+with open(os.path.join('./results/compactor_tests','0.cc'), 'r') as file:
+    lines = file.readlines();
+
+    #### COMPRESSING EQUAL CODE LINES INTO A FOR LOOP
+    loop_lines = []
+    prev = None
+    # curr = None
+    c = 0
+    for line in lines:
+        # print("not stripped: {}".format(line))
+        # print("stripped: {}".format(line.strip()))
+        # print("loop_lines: {} prev: {}".format(loop_lines, prev))
+        line = line.strip()
+        if prev == line:
+            loop_lines.append(line)
+        elif line != prev and len(loop_lines) > 0:
+            output_lines.append("""\nfor (int i = 0; i < {0}; i++) {{ \n\t{1}\n}}\n\n""".format(len(loop_lines)+1, loop_lines[0]))
+            # print("for (int i = 0; i < 2; i++) \{ \}")
+            output_lines.append("{}\n".format(line))
+            loop_lines = []
+            prev = None
+            continue
+        elif prev is not None and len(loop_lines) == 0:
+            output_lines.append("{}\n".format(prev))
+        if line != '':
+            prev = line
+        else:
+            prev = None
+    output_lines.append("{}\n".format(prev))
+
+    #### GATHERING VARIABLES NAME
+    output_lines_tmp = output_lines.copy()
+    output_lines = []
+    variables = []
+    for line in output_lines_tmp:
+        declaring_int = re.search(REGEX_DECLARE_INT, line)
+        # print(declaring_int)
+        if declaring_int:
+            name = declaring_int.group()
+            if name != 'i':
+                variables.append(name)
+            # variables.append()
+        declaring_float = re.search(REGEX_DECLARE_FLOAT, line)
+        # print(declaring_float)
+        if declaring_float:
+            name = declaring_float.group()
+            if name != 'i':
+                variables.append(name)
+    print("variables: {}".format(variables))
+
+    #### REMOVE IF UNUSED
+    variables_to_use = []
+    for var in variables:
+        for line in output_lines_tmp:
+            usage_assignment = re.search(REGEX_USAGE_ASSIGNMENT, line)
+            if usage_assignment:
+                variables_to_use.append(var)
+                continue
+            
+            usage_ifthenelse = re.search(REGEX_USAGE_IF_THEN_ELSE, line)
+            if usage_ifthenelse:
+                variables_to_use.append(var)
+                continue
+    print("variables to use: {}".format(variables_to_use))
+
+    for var in variables:
+        if var not in variables_to_use:
+            for line in output_lines_tmp:
+
+                var_contained = re.search(var, line)
+                if var_contained:
+                    continue
+
+                output_lines.append(line)
+            output_lines_tmp = output_lines.copy()
+            output_lines = []
+    
+    output_lines = output_lines_tmp.copy()
+    output_lines_tmp = []
+
+    ### CLEAN EMPTY IF THEN ELSE
+    prev = None
+    for idx, line in enumerate(output_lines):
+        if prev is not None and prev.strip() == '} else {' and line.strip() == '}':
+            prev = None
+            continue
+        elif re.match(REGEX_IS_IF, line.strip()) and idx < len(output_lines)-1 and output_lines[idx+1].strip() == '} else {':
+            prev = None
+            continue
+        elif prev is not None:
+            output_lines_tmp.append(prev)
+        
+        if line.strip() != '':
+            prev = line
+        else:
+            prev = None
+    
+    output_lines = output_lines_tmp.copy()
+    output_lines_tmp = []
+    
+    ### SIMPLIFY EXPRESSION
+    for idx, line in enumerate(output_lines):
+        line_tmp = line
+        matches = re.search(REGEX_EXPRESSION, line_tmp.strip())
+        if matches:
+            match = matches.group()
+            # print(match, eval(match))
+            line_tmp = line_tmp.replace(str(match), str(eval(match)))
+            output_lines_tmp.append(line_tmp)
+            # n = len(matches)
+            # print("matches {}".format(matches))
+            # for i, match in enumerate(matches):
+            #     print("{} match: {}".format(i, match))
+                # if n == 1:
+                #     res = eval(match[1:])
+        else:
+            output_lines_tmp.append(line)
+
+    
+    output_lines = output_lines_tmp.copy()
+    output_lines_tmp = []
+
+with open(os.path.join('./results/compactor_tests','0_compacted.cc'), 'w+') as file:
+    file.writelines(output_lines)
